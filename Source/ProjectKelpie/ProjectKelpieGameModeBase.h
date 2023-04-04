23d9256a577fd3e168a87264db68a83ec1c0671d
Dustin Biser <dustinbiser@gmail.com>
2023-04-03 21:45:15 -0700

    First project commit

---
+++ Source/ProjectKelpie/ProjectKelpieGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectKelpieGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTKELPIE_API AProjectKelpieGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
